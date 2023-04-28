4e71a3e300f436b9fe2e8255a3e2524368cdadb4
Dreher <dreheren20@gcc.edu>
2023-04-27 21:15:18 -0400

    restructuring project to be good for springboot

--- gs-spring-boot/complete/src/main/java/com/example/springboot/ScrumlingsController.java
+++ gs-spring-boot/complete/src/main/java/com/example/springboot/ScrumlingsController.java
@@ -1,33 +1,43 @@
 package com.example.springboot;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-@RestController
+@Controller
 public class ScrumlingsController {
     public static String renderTemplate(String name) {
         try {
-            File f = new File("../../html/" + name);
+            File f = new File(name);
             Scanner fs = new Scanner(f);
             StringBuilder out = new StringBuilder();
             while(fs.hasNext()) {
-                out.append(fs.next());
+                out.append(fs.next() + "\n");
             }
             return out.toString();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            return System.getProperty("user.dir");
+            return "ERROR, " + name + " not found in directory.";
         }
     }
 
     @GetMapping("/")
-    public String index() {
-        return renderTemplate("home.html");
+    public RedirectView index() {
+        // TODO redirect to login page maybe?
+        return new RedirectView("/home");
     }
 
-    
+    @GetMapping("/home")
+    public String getHome() { return "home.html"; }
 
+    @GetMapping("/courses")
+    public String getCourses() {
+        return "testing";
+    }
 }
---
+++ gs-spring-boot/complete/src/main/resources/static/home.js
@@ -0,0 +1,8 @@
+addEventListener("DOMContentLoaded", () => {
+    console.log("hello world");
+}) 
+
+//export const getData = async () => {
+//    response = await axios.get("https://localhost:8080/courses");
+//    return response.data;
+//}
