a467c29ab145c09a353ce33cd44b1ddd6678a2c7
avaHatfield <106991181+avaHatfield@users.noreply.github.com>
2023-05-02 18:41:44 -0400

    calender

--- src/pages/View.js
+++ src/pages/View.js
@@ -1,12 +1,18 @@
 import Table from '../component/Table';
 import React, { Component } from "react";
 import {useNavigate} from "react-router-dom";
+import {DayPilot, DayPilotCalendar} from "@daypilot/daypilot-lite-react";
 
 const View = () => {
     return (
         <div className="Home">
             <h4 class = "pad">My Schedule</h4>
-            <Table></Table>
+
+            <DayPilotCalendar
+                  viewType={"WorkWeek"}
+             />
+
+
         </div>
     )
 }
