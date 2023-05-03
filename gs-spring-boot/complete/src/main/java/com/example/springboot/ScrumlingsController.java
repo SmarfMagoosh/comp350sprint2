25f69b89a870ab42931b592ada30d3614124bff8
ALLARASSEMJJ20 <ALLARASSEMJJ20@5CG024DG3X>
2023-05-03 16:22:52 -0400

    first harvest

--- gs-spring-boot/complete/src/main/java/com/example/springboot/ScrumlingsController.java
+++ gs-spring-boot/complete/src/main/java/com/example/springboot/ScrumlingsController.java
@@ -1,16 +1,74 @@
 package com.example.springboot;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.HashMap;
 
 @Controller
 @CrossOrigin(origins = "http://localhost:3000")
 public class ScrumlingsController {
+
+    Boolean isAuth = false;
     @GetMapping("/courses")
     public String getCourses() {
         return "courses.json";
+    }
+
+    public HashMap<String, String> CredentialsJSONParser (String data){
+        //remove {}
+        String temp1 = data.substring(1, data.length()-1);
+        //split all elements keys and values in buckets
+        String [] tempArr1 = temp1.split(",|:");
+        //make the hashmap
+        HashMap<String, String> map = new HashMap<>();
+        for (int i =0; i < tempArr1.length; i+=2){
+            String key = tempArr1[i].trim();
+            String value = tempArr1[i + 1].trim();
+            map.put(key, value);
+        }
+        return map;
+    }
+
+    @RequestMapping("/register")
+    public ResponseEntity<String> Register(@RequestBody String data) throws SQLException {
+
+        // extract the information
+        System.out.println(data);
+        HashMap<String, String> map = CredentialsJSONParser(data);
+        System.out.println(map);
+        String username = map.get("username");
+        String password = map.get("password");
+
+//
+//        // store in database
+//        Connection connection = DriverManager.getConnection(jdbcURL);
+//        String sql = "SELECT * FROM users";
+//        Statement statement = connection.createStatement();
+//        ResultSet result = statement.executeQuery(sql);
+//        System.out.println(result);
+        return ResponseEntity.ok("registering successful");
+    }
+
+
+    public void setAuth(boolean dummyAuth){
+        this.isAuth = dummyAuth;
+    }
+
+    @RequestMapping("/login")
+    public ResponseEntity<String> Login(@RequestBody String data) {
+
+        // check against database
+        HashMap<String, String> map = CredentialsJSONParser(data);
+        String username = map.get("username");
+        String password = map.get("password");
+
+
+        // set auth to true if logged in otherwise false
+        this.setAuth(true);
+
+        return ResponseEntity.ok("login successful");
     }
 }
