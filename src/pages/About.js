fb23fcd6f609447c0b7b8eca3c6c42006cbab3f6
avaHatfield <106991181+avaHatfield@users.noreply.github.com>
2023-05-09 18:12:32 -0400

    fix create schedule bug

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,41 +1,49 @@
 import React, { Component } from 'react'
 import "./About.css";
-//import Card from '../component/Card';
+//import Schedule from '../pages/Home';
 import instance from '../component/api/axios-config';
 
 //import croppedprofile from "../assets/croppedprofile.jpg";
 
 export default class About extends Component {
+    constructor() {
+      super()
+        this.createSchedule = this.createSchedule.bind(this)
+    }
+
+    createSchedule(event) {
+        let name = document.getElementById("schedName").value;
+        let schedule = {
+          "name": name,
+          "term": "",
+          "courses": []
+        };
+        return new Schedule(schedule);
+    }
+
 
 
   render() {
     return (
       <div>
         <div>
-
-      <div className="centered">
-      </div>
-     <div className="centered">
-	        <div className="name_title">My Schedules</div>
-
-            <div>
-                    <h5>Create Schedule</h5>
-                        <label>Schedule Name: </label>
-                                <input type = "text" name = "sch" ></input>
-                         <div>
-                        <label>Term: </label>
-                        <input type = "text" name = "sch" ></input>
-                         </div>
-                       <button onClick = { this.addCourse }>Create Schedule</button>
-                       <div class ="padsm"> </div>
-                    </div>
-
-
-
-
-      </div>
-
-  </div>
+          <div class = "centered"></div>
+        </div>
+        <div class = "centered">
+          <div class = "name_title">
+            My Schedules
+          </div>
+          <h5>Create Schedule</h5>
+          <label>Schedule Name: </label>
+          <input type = "text" name = "sched" id = "schedName"></input>
+          <div>
+            <button onClick= { this.createSchedule }>Create Schedule</button>
+            <div class = "padsm"></div>
+          </div>
+          <div>
+         
+          </div>
+        </div>
       </div>
     )
   }
--- src/pages/View.js
+++ src/pages/View.js
@@ -7,6 +7,7 @@ const View = () => {
     return (
         <div className="Home">
             <h1 class = "pad">Courses Calendar View</h1>
+
                         <h3> Monday &emsp; &emsp; &emsp;  Tuesday &emsp; &emsp;&emsp;    Wednesday &emsp; &emsp;  &emsp; Thursday &emsp; &emsp; &emsp;  Friday</h3>
 
                         <Calendar/>
